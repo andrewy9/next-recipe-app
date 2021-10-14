2b4c0c9f5ff1168629f81255fe96121dc0bde4ee
andrewy9 <andrew.yang993@gmail.com>
2021-10-15 09:27:14 +1300

    committing

---
+++ pages/recipes/[slug].js
@@ -0,0 +1,57 @@
+import {
+  sanityClient,
+  urlFor,
+  usePreviewSubscription,
+  PortableText
+} from "../../lib/sanity";
+
+const recipeQuery = `*[_type == "recipe" && slug.current == $slug][0]{
+  _id,
+  name,
+  slug,
+  mainImage,
+  ingredient[]{
+    unit,
+    wholeNumber,
+    fraction,
+    ingredient->{
+      name
+    }
+  },
+  instructions
+}`;
+
+export default function OneRecipe({ data }) {
+  const { recipe } = data;
+  return (
+    <article>
+    </article>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = await sanityClient.fetch(
+    `*[_type == "recipe" && defined(slug.current)]{
+      "params": {
+        "slug": slug.current
+      }
+    }`
+  )
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const recipe = await sanityClient.fetch(recipeQuery, { slug })
+  return {
+    props:
+    {
+      data: {
+        recipe
+      }
+    }
+  }
+}
